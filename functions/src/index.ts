ac2bcba6c37954b254d8ff57e4cbdce243059132
tksgi <23550705+tksgi@users.noreply.github.com>
2020-04-07 13:37:24 +0900

    firestoreへの書き込み

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,19 @@
 import * as functions from 'firebase-functions';
 
+// firestoreに接続するためにfirebase-adminを利用する
+import * as admin from 'firebase-admin';
+admin.initializeApp();
+
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 
 export const helloWorld = functions.https.onRequest((request, response) => {
  response.send("Hello from Firebase!");
+});
+
+// https://github.com/firebase/functions-samples/blob/master/quickstarts/uppercase-firestore/functions/index.js
+export const addMessage = functions.https.onRequest(async (req, res) => {
+  const original = req.query.text;
+  const writeResult = await admin.firestore().collection('messages').add({original: original});
+  res.json({result: `Message with ID: ${writeResult.id} added.`});
 });
