d97609fb9e31667a32ea07e06b3c14f8347fb697
tksgi <23550705+tksgi@users.noreply.github.com>
2020-04-07 14:31:50 +0900

    firestoreへのデータ追加でトリガーされる関数を追加

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -12,8 +12,19 @@ export const helloWorld = functions.https.onRequest((request, response) => {
 });
 
 // https://github.com/firebase/functions-samples/blob/master/quickstarts/uppercase-firestore/functions/index.js
+// リクエストのtextパラメータの値をfirestoreに格納
 export const addMessage = functions.https.onRequest(async (req, res) => {
   const original = req.query.text;
   const writeResult = await admin.firestore().collection('messages').add({original: original});
   res.json({result: `Message with ID: ${writeResult.id} added.`});
 });
+
+// firestoreに値が格納されたタイミングでトリガーされる
+// 格納された値を大文字に変換してuppercaseフィールドに格納
+exports.makeUppercase = functions.firestore.document('/messages/{documentId}')
+                                                     .onCreate((snap, context) => {
+                                                       const original = snap.data()!.original;
+                                                       console.log('Uppercasing', context.params.documentId, original);
+                                                       const uppercase = original.toUpperCase();
+                                                       return snap.ref.set({uppercase}, {merge: true});
+                                                     });
